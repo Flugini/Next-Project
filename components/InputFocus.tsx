48fe7bda27ea546eb88d5361b11fe2960c93cea8
Zana Flugini <zanaflugu@gmail.com>
2024-08-06 11:46:37 +0200

    Input Focus Change

--- components/InputFocus.tsx
+++ components/InputFocus.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 
 const InputWithFocus: React.FC = () => {
     const inputElement = useRef<HTMLInputElement>(null)
 
     const onFocusClick = () => {
         if(inputElement && inputElement.current) {
-            inputElement.current.focus()
+            inputElement.current.focus();
         }
 
-    }
+    };
 
     return (
         <div>
-            <input type="text" />
+            <h2>focus element</h2>
+            <input type="text" ref={inputElement} />
             <button onClick={onFocusClick}>Focus the Input</button>
         </div>
     )
